48b4f5d14eeef6b9cc0947f73868383bbcd47fc3
RandomeMar <mauricio.a.alv@gmail.com>
2025-12-01 18:36:59 -0800

    Added admin utility for messages

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Listing, ListingImage
+from .models import Listing, ListingImage, Purchase, Message
 
 # Register your models here.
 admin.site.register(ListingImage)
@@ -8,3 +8,13 @@ admin.site.register(ListingImage)
 class ListingAdmin(admin.ModelAdmin):
     list_display = ("title", "product", "owner")
     search_fields = ("title", "owner__username")
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ("listing", "seller", "buyer", "status")
+    search_fields = ("is_flagged",)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("sender", "receiver", "listing", "timestamp")
+    search_fields = ("message_text",)
